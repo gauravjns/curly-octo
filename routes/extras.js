a224bd82b63bff6131e67b4ec9c925259d7698c5
root <gauravjn.iitd@gmail.com>
2015-10-18 19:31:52 +0530

    First commit

--- routes/extras.js
+++ routes/extras.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // respond with "Hello World!" on the homepage
 router.get('/about', function (req, res) {
-  res.render('Hello World! You are at home');
+  res.render('about', { title: 'Express' });
 });
 
 module.exports = router;
